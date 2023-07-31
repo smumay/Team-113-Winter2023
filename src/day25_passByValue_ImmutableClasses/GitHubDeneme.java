afb361b276405a6731b7c33fc2871945aa0d208c
Your Name <you@example.com>
2023-07-31 09:56:22 +0200

    20230731.v1

---
+++ src/day25_passByValue_ImmutableClasses/GitHubDeneme.java
@@ -0,0 +1,16 @@
+package day25_passByValue_ImmutableClasses;
+
+public class GitHubDeneme {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("GitHub i ogrenmem gerekiyor...");
+
+        if (5>3){
+            System.out.println("dogru islem");
+        }else {
+
+        }
+    }
+}
